24f0359a6d4c906cd6994a0fba38029ca3eac2cc
Hongsheng Liu <hongs.liu@outlook.com>
2020-12-31 16:40:24 +0800

    checkpoint

--- main_work.R
+++ main_work.R
@@ -10,7 +10,7 @@ GM <- st_read(here::here("GM", "GM.shp")) %>%
 Obesity <- read_csv(here::here("data", "child_obesity_data", "child_obesity_data.csv"),
                      na = c("NA", "n/a")) %>% 
   clean_names() %>%
-  mutate(across(.cols = 5:8, as.numeric))
+  mutate(across(.cols = 5:6, as.numeric))
 
 Poverty <- read_csv(here::here("data", "poverty_data", "households_in_poverty_14.csv"),
                     na = c("NA", "n/a")) %>% 
@@ -25,7 +25,7 @@ GM_joined <- GM %>%
 GM_joined <- GM_joined %>%
   left_join(.,
             Poverty,
-            by= c("msoa11cd" = "msoa_code"))%>%
+            by= c("msoa11cd" = "msoa_code"))
 
 drop.cols <- c('msoa11nmw',
                'msoa_name.x',
@@ -34,6 +34,7 @@ drop.cols <- c('msoa11nmw',
                'la_name.y')
 
 GM_joined_cleaned <- GM_joined %>% select(-one_of(drop.cols))
+
 rm(Obesity, Poverty)
 
 GMlist <- c('Manchester',
@@ -47,7 +48,7 @@ GMlist <- c('Manchester',
             'Salford',
             'Trafford')
 
-LA_cases_overweight <- read_csv(here::here("data", "child_obesity_data", "LA_cases_overweight.csv"),
+LA_cases_excessweight <- read_csv(here::here("data", "child_obesity_data", "LA_cases_overweight.csv"),
                     na = c("NA", "n/a")) %>% 
   clean_names() %>%
   mutate(across(.cols = 3:4, as.numeric))%>%
@@ -59,8 +60,9 @@ LA_cases_obese <- read_csv(here::here("data", "child_obesity_data", "LA_cases_ob
   mutate(across(.cols = 3:4, as.numeric))%>%
   filter(area_name %in% GMlist)
 
-GM_overweight_rate = (sum(LA_cases_overweight$numerator)/sum(LA_cases_overweight$denominator)) *100
+GM_excessweight_rate = (sum(LA_cases_excessweight$numerator)/sum(LA_cases_excessweight$denominator)) *100
 GM_obese_rate = (sum(LA_cases_obese$numerator)/sum(LA_cases_obese$denominator)) *100
 
-GM_joined_cleaned <- GM_joined_cleaned %>%
-  mutate(sir_year6_overweight = )
+GM_joined_cleaned_SIR <- GM_joined_cleaned %>%
+  mutate(sir_year6_excessweight = year6_excessweight_rate/GM_excessweight_rate) %>%
+  mutate(sir_year6_obese = year6_obese_rate/GM_obese_rate) 
