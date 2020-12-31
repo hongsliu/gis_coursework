11b956c631cee7937e5c29bb5adc01046d9f7740
Hongsheng Liu <hongs.liu@outlook.com>
2020-12-31 22:08:03 +0800

    removed SIR

--- main_work.R
+++ main_work.R
@@ -39,35 +39,7 @@ GM_joined_cleaned <- GM_joined %>% select(-one_of(drop.cols))
 
 rm(Obesity, Poverty)
 
-GMlist <- c('Manchester',
-            'Stockport',
-            'Tameside',
-            'Oldham',
-            'Rochdale',
-            'Bury',
-            'Bolton',
-            'Wigan',
-            'Salford',
-            'Trafford')
-
-LA_cases_excessweight <- read_csv(here::here("data", "child_obesity_data", "LA_cases_overweight.csv"),
-                    na = c("NA", "n/a")) %>% 
-  clean_names() %>%
-  mutate(across(.cols = 3:4, as.numeric))%>%
-  filter(area_name %in% GMlist)
-
-LA_cases_obese <- read_csv(here::here("data", "child_obesity_data", "LA_cases_obese.csv"),
-                                na = c("NA", "n/a")) %>% 
-  clean_names() %>%
-  mutate(across(.cols = 3:4, as.numeric))%>%
-  filter(area_name %in% GMlist)
-
-GM_excessweight_rate = (sum(LA_cases_excessweight$numerator)/sum(LA_cases_excessweight$denominator)) *100
-GM_obese_rate = (sum(LA_cases_obese$numerator)/sum(LA_cases_obese$denominator)) *100
-
 joined <- GM_joined_cleaned %>%
-  mutate(sir_year6_excessweight = year6_excessweight_rate/GM_excessweight_rate) %>%
-  mutate(sir_year6_obese = year6_obese_rate/GM_obese_rate) %>%
   mutate(log_poverty_ahc_rate = log(poverty_ahc_rate))
 
 qtm(joined, fill = "year6_obese_rate")
